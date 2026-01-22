0c4514bcc8e3ea0e352e2b43c94de9a85f5dae8d
AbnadabyBonaparte <abnadabybonaparte@users.noreply.github.com>
2026-01-22 15:20:09 -0300

    Simplificar funÃ§Ã£o serverless do Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,12 @@
 // Vercel serverless function entry point
 import { createApp } from "../server/_core/index.js";
 
+let appInstance: any = null;
+
 export default async function handler(req: any, res: any) {
-  const app = await createApp();
-  return app(req, res);
+  if (!appInstance) {
+    appInstance = await createApp();
+  }
+  return appInstance(req, res);
 }
 
